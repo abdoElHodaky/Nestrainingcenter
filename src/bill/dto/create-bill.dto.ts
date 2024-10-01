829a8073fd4e238f084ec2e29a927e52cfaf845e
AbdElrhman ElHodaky <abdo.arh38@yahoo.com>
2024-10-01 20:59:28 +0300

    Update create-bill.dto.ts

--- src/bill/dto/create-bill.dto.ts
+++ src/bill/dto/create-bill.dto.ts
@@ -1,34 +1,46 @@
 import { IsNotEmpty, IsString } from 'class-validator';
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+
 
 export class CreateBillDto {
+  
   @IsNotEmpty()
   @IsString()
+  @ApiProperty()
   type: string;
 
   @IsNotEmpty()
   @IsString()
+  @ApiProperty()
   title: string;
 
   @IsString()
+  @ApiProperty()
   sender: string;
 
   @IsString()
+  @ApiProperty()
   receiver: string;
 
   @IsString()
+  @ApiPropertyOptional()
   description: string;
 
   @IsNotEmpty()
   @IsString()
+  @ApiProperty()
   status: string;
 
   @IsNotEmpty()
+  @ApiProperty()
   amount: number;
 
   @IsNotEmpty()
   @IsString()
+  @ApiProperty()
   paymentMethod: string;
 
   @IsNotEmpty()
+  @ApiProperty()
   date: Date;
 }
