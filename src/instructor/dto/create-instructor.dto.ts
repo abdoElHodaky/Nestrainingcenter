091f8846435be89991c7b415eb54cdef16b53281
AbdElrhman ElHodaky <abdo.arh38@yahoo.com>
2024-10-01 21:05:36 +0300

    Update create-instructor.dto.ts

--- src/instructor/dto/create-instructor.dto.ts
+++ src/instructor/dto/create-instructor.dto.ts
@@ -8,6 +8,12 @@ import {
   MaxLength,
   MinLength,
 } from 'class-validator';
+
+import { 
+  ApiProperty, 
+  ApiPropertyOptional
+} from '@nestjs/swagger';
+
 import { Gender } from 'src/gender.enum';
 
 export class CreateInstructorDto {
@@ -15,49 +21,61 @@ export class CreateInstructorDto {
   @IsNotEmpty()
   @MinLength(2)
   @MaxLength(20)
+  @ApiProperty()
   firstName: string;
 
   @IsString()
   @IsNotEmpty()
   @MinLength(2)
   @MaxLength(20)
+  @ApiProperty()
   lastName: string;
 
   @IsString()
   @Length(2)
   @IsNotEmpty()
+  @ApiProperty()
   age: string;
 
   @IsNotEmpty()
+  @ApiProperty()
   gender: Gender;
 
   @IsNotEmpty()
   @MaxLength(50)
+  @ApiProperty()
   address: string;
 
   @IsNotEmpty()
   @Length(8)
+  @ApiProperty()
   CIN: string;
 
   @IsNotEmpty()
   @MaxLength(20)
-  @IsPhoneNumber('TN')
+  @IsPhoneNumber('EG')
+  @ApiProperty()
   phoneNumber: string;
 
   @IsEmail()
   @MaxLength(50)
   @IsNotEmpty()
+  @ApiProperty()
   email: string;
 
   @IsNotEmpty()
+  @ApiProperty()
   highestEducationLevel: string;
 
   @IsNotEmpty()
+  @ApiProperty()
   educationalInstitution: string;
 
   @IsNotEmpty()
+  @ApiProperty()
   major: string;
 
   @IsNotEmpty()
+  @ApiProperty()
   city: string;
 }
