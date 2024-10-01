836717d97529868f2b60d7d1da516183e41b21e5
AbdElrhman ElHodaky <abdo.arh38@yahoo.com>
2024-10-01 20:49:57 +0300

    Update auth-credentials.dto.ts

--- src/auth/dto/auth-credentials.dto.ts
+++ src/auth/dto/auth-credentials.dto.ts
@@ -6,19 +6,28 @@ import {
   MinLength,
 } from 'class-validator';
 
+import { 
+  ApiProperty, 
+  ApiPropertyOptional 
+} from '@nestjs/swagger';
+
 export class AuthCredentialsDto {
+  
   @IsEmail()
   @MaxLength(50)
   @IsNotEmpty()
+  @ApiProperty()
   email: string;
 
   @IsString()
   @MinLength(4)
   @MaxLength(20)
+  @ApiProperty()
   username: string;
 
   @IsString()
   @MinLength(4)
   @MaxLength(20)
+  @ApiProperty()
   password: string;
 }
