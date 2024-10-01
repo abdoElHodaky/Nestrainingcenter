924b16d0aba89e32abd1e281348183b59ec48a14
AbdElrhman ElHodaky <abdo.arh38@yahoo.com>
2024-10-01 20:52:35 +0300

    Update create-admin.dto.ts

--- src/auth/dto/create-admin.dto.ts
+++ src/auth/dto/create-admin.dto.ts
@@ -9,35 +9,48 @@ import {
   MinLength,
 } from 'class-validator';
 
+import { 
+  ApiProperty, 
+  ApiPropertyOptional 
+} from '@nestjs/swagger';
+
+
 export class CreateAdminDto {
+  
   @IsString()
   @MinLength(2)
   @MaxLength(20)
+  @ApiProperty()
   firstName: string;
 
   @IsString()
   @MinLength(2)
   @MaxLength(20)
+  @ApiProperty()
   lastName: string;
 
   @IsString()
   @MinLength(2)
   @MaxLength(20)
+  @ApiProperty()
   username: string;
 
   @IsString()
   @Length(2)
   @IsNotEmpty()
+  @ApiProperty()
   age: string;
 
   @IsNotEmpty()
   @MaxLength(20)
-  @IsPhoneNumber('TN')
+  @IsPhoneNumber('EG')
+  @ApiProperty()
   phoneNumber: string;
 
   @IsEmail()
   @MaxLength(50)
   @IsNotEmpty()
+  @ApiProperty()
   email: string;
 
   @IsString()
@@ -46,5 +59,6 @@ export class CreateAdminDto {
   @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
     message: 'password is too weak',
   })
+  @ApiProperty()
   password: string;
 }
