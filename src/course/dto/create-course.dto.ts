deaaafe7194e52c5d30b69918c99170911b6108b
AbdElrhman ElHodaky <abdo.arh38@yahoo.com>
2024-10-01 21:02:12 +0300

    Update create-course.dto.ts

--- src/course/dto/create-course.dto.ts
+++ src/course/dto/create-course.dto.ts
@@ -1,45 +1,58 @@
 import { IsNotEmpty, IsNumber, IsDate, IsString } from 'class-validator';
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 
 export class CreateCourseDto {
+  
   @IsNotEmpty()
   @IsString()
+  @ApiProperty()
   title: string;
 
   @IsNotEmpty()
   @IsString()
+  @ApiProperty()
   description: string;
 
   @IsNotEmpty()
   @IsString()
+  @ApiProperty()
   category: string;
 
   @IsNotEmpty()
   @IsString()
+  @ApiProperty()
   duration: string;
 
   @IsNotEmpty()
+  @ApiProperty()
   startDate: string;
 
   @IsNotEmpty()
+  @ApiProperty()
   endDate: string;
 
   @IsNotEmpty()
   @IsNumber()
+  @ApiProperty()
   maxParticipants: number;
 
   @IsNotEmpty()
   @IsString()
+  @ApiProperty()
   prerequisites: string;
 
   @IsNotEmpty()
   @IsNumber()
+  @ApiProperty()
   registrationFee: number;
 
   @IsNotEmpty()
   @IsNumber()
+  @ApiProperty()
   instructorSalary: number;
 
   @IsNotEmpty()
   @IsNumber()
+  @ApiProperty()
   instructorId: number;
 }
