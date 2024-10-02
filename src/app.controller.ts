f874176b84bd8065e77fa8133661c1cc43704a1d
AbdElrhman ElHodaky <abdo.arh38@yahoo.com>
2024-10-02 08:16:39 +0300

    Update app.controller.ts

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,17 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ApiExcludeController } from "@nestjs/swagger";
 
+//@UseGuards(OptionalJwtAuthGuard)
+@ApiExcludeController()
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
-  }
+  constructor() {}
+  
+  @Redirect("/docs")
+  @Get("")
+  async index(@Res() res:Response ){   
+    
+    
+   }
 }
