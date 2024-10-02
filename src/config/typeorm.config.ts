daaaf9eb9eafc022d5efc376793fe905859e087c
AbdElrhman ElHodaky <abdo.arh38@yahoo.com>
2024-10-02 09:21:56 +0300

    Update typeorm.config.ts

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -8,14 +8,18 @@ import { Participant } from 'src/participant/entities/participant.entity';
 const database_url="postgresql://nestjs-training-center_owner:qr4BigjP1WUz@ep-billowing-mud-a1s0uks8.ap-southeast-1.aws.neon.tech/nestjs-training-center?sslmode=require"
 var parse = require('pg-connection-string').parse;
 
-var config = parse('postgres://someuser:somepassword@somehost:381/somedatabase')
+var connectionOptions = parse(`${database_url}`)
 export const typeOrmConfig: TypeOrmModuleOptions = {
   type: 'postgres',
-  /*host: 'localhost',
-  port: 1919,
-  username: 'postgres',
-  password: 'admin',
-  database: 'horizondata',*/
+  name: connectionOptions.name,
+  host: connectionOptions.host,
+  port: connectionOptions.port,
+  username: connectionOptions.username,
+  password: connectionOptions.password,
+  database: connectionOptions.database,
   entities: [Instructor, Course, Material, Participant, Bill, User],
   synchronize: true,
+  extra:{
+    ssl:true
+  }
 };
