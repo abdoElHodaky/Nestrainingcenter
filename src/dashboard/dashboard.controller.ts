0c53ef2ce8c846f9096fd92aae3357e50dc8ef4e
AbdElrhman ElHodaky <abdo.arh38@yahoo.com>
2024-10-02 22:28:41 +0300

    Update dashboard.controller.ts

--- src/dashboard/dashboard.controller.ts
+++ src/dashboard/dashboard.controller.ts
@@ -1,6 +1,8 @@
 import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
 import { DashboardService } from './dashboard.service';
+import { ApiTags } from "@nestjs/swagger";
 
+@ApiTags("Dashboard")
 @Controller('dashboard')
 export class DashboardController {
   constructor(private readonly dashboardService: DashboardService) {}
